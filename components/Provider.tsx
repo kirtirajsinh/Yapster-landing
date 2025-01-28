8b0e7d7e5f98c1e00ec61765d46d6ee153b74f48
kirtirajsinh <kirtiraj3738@gmail.com>
2025-01-28 23:12:11 +0530

    Add Provider component and Zustand store for user management

---
+++ components/Provider.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useEffect } from "react";
+import FrameSDK from "@farcaster/frame-sdk";
+import { Toaster } from "react-hot-toast";
+import { useFrameStore } from "./useStore";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  const [isSDKLoaded, setIsSDKLoaded] = React.useState(false);
+  const { setUser } = useFrameStore();
+  useEffect(() => {
+    const load = async () => {
+      console.log("Running Frame Action ready");
+      // Add the FrameSDK.actions.ready() otherwise your app will get stuck in a loading state i.e. a Splash screen.
+      FrameSDK.actions.ready();
+      const frameuser = await FrameSDK.context;
+      console.log("Frame Action ready", frameuser);
+      if (frameuser?.user) {
+        setUser({
+          displayName: frameuser?.user.displayName || "",
+          fid: frameuser?.user.fid,
+          location: frameuser?.user.location || {
+            placeId: "",
+            description: "",
+          },
+          pfpUrl: frameuser?.user.pfpUrl || "",
+          username: frameuser?.user.username || "",
+        });
+      } else {
+        console.warn(
+          "No user found in FrameSDK context or displayName is undefined."
+        );
+      }
+    };
+    if (FrameSDK && !isSDKLoaded) {
+      setIsSDKLoaded(true);
+      load();
+    }
+  }, [isSDKLoaded]);
+  return (
+    <div>
+      {children}
+      <Toaster />
+    </div>
+  );
+};
+
+export default Provider;
