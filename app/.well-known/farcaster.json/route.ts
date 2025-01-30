639245d27f5e2d72c1538b41dc74862b9e1e28ea
kirtirajsinh <kirtiraj3738@gmail.com>
2025-01-30 10:25:58 +0530

    Refactor Farcaster configuration remove "" wherever not needed.

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,20 +1,20 @@
 export async function GET() {
     const config = {
         "accountAssociation": {
-            "header": "eyJmaWQiOjY4NjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhBNTkzMUE3MjZDRUNjYTA1N0EzRkY0M0E4NDg4MzQ2NjI2MDQ3OEI0In0",
-            "payload": "eyJkb21haW4iOiJ5YXBzdGVyLmZ1biJ9",
-            "signature": "MHhiNDM1NDhiM2Q1OGQ0ZDIxZGYxMmRhYmJhYWZkZDE0ZDdlNmE2NjI5MDNiOTYzNTE4NDBiYjI3NDBkZTAwMDYyMGZmZGU0YzEzMTM2OTNhOGFmNWY2NzhiN2E2Zjk1NGIyODQ3MWUwNzY4MGFkYjNlMzYyOTUwNGY5MGRkZWY0MzFi"
+            header: "eyJmaWQiOjY4NjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhBNTkzMUE3MjZDRUNjYTA1N0EzRkY0M0E4NDg4MzQ2NjI2MDQ3OEI0In0",
+            payload: "eyJkb21haW4iOiJ5YXBzdGVyLmZ1biJ9",
+            signature: "MHhiNDM1NDhiM2Q1OGQ0ZDIxZGYxMmRhYmJhYWZkZDE0ZDdlNmE2NjI5MDNiOTYzNTE4NDBiYjI3NDBkZTAwMDYyMGZmZGU0YzEzMTM2OTNhOGFmNWY2NzhiN2E2Zjk1NGIyODQ3MWUwNzY4MGFkYjNlMzYyOTUwNGY5MGRkZWY0MzFi"
         },
-        "frame": {
-            "version": "1",
-            "name": "Yapster",
-            "iconUrl": "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/tinylogo.svg",
-            "homeUrl": "https://yapster.fun",
-            "imageUrl": "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/newimage.svg",
-            "buttonTitle": "Start Yapping",
-            "splashImageUrl": "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/tinylogo.svg",
-            "splashBackgroundColor": "#000000",
-            "webhookUrl": "https://yapster.fun/api/webhook"
+        frame: {
+            version: "1",
+            name: "Yapster",
+            iconUrl: "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/tinylogo.svg",
+            homeUrl: "https://yapster.fun",
+            imageUrl: "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/newimage.svg",
+            buttonTitle: "Start Yapping",
+            splashImageUrl: "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/tinylogo.svg",
+            splashBackgroundColor: "#000000",
+            webhookUrl: "https://yapster.fun/api/webhook"
         }
     }
 
